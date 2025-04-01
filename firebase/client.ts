1de1226a83dcad3463ca8cdd1d7b6b25ad8b412d
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-04-01 17:17:55 +0530

    Firebase Setup

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp  , getApps ,getApp } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCy8YLRHcRiixNP3GkSd8m_9FL7jQ0b1o",
+  authDomain: "prepwise-ab389.firebaseapp.com",
+  projectId: "prepwise-ab389",
+  storageBucket: "prepwise-ab389.firebasestorage.app",
+  messagingSenderId: "819498144866",
+  appId: "1:819498144866:web:c21b13703943d4e3972d3b",
+  measurementId: "G-7TVYMZ2XTF"
+};
+
+const app = !getApps.length ?  initializeApp(firebaseConfig) : getApp();
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
